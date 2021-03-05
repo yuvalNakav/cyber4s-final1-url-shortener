12e3412532709c1a133b59604c76dc474d6106bf
Yuval Nakav <yuvalnakav1@gmail.com>
2021-03-05 23:55:53 +0200

    added checkInWeb middleware to check the url's validity. added node-fetch

--- routes/shortUrl/index.js
+++ routes/shortUrl/index.js
@@ -1,24 +1,26 @@
 const { Console } = require("console");
+const { response } = require("express");
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const path = require("path");
 const DataBase = require("../../models/dataBaseObj");
 router.use(express.json());
+const fetch = require("node-fetch");
+const { resolve } = require("path");
 
 router.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "../../views") });
 });
-router.get("/new", (req, res) => {});
 
-router.post("/new", checkExistence, (req, res) => {
+router.post("/new", checkInBin, checkInWeb, (req, res) => {
   const { body } = req;
   const url = body.url;
   fs.readFile(`bins/bin.json`, (err, success) => {
     if (err) {
       console.log(err);
     } else {
-      let binArr = JSON.parse(success);
+      const binArr = JSON.parse(success);
       const urlObject = new DataBase(url, binArr.length + 1);
       binArr.push(urlObject);
       fs.writeFile(`bins/bin.json`, JSON.stringify(binArr, null, 4), (err) => {
@@ -37,25 +39,45 @@ router.post("/new", checkExistence, (req, res) => {
     }
   });
 });
-function checkExistence(req, res, next) {
+function checkInBin(req, res, next) {
   const { body } = req;
   const url = body.url;
   fs.readFile(`bins/bin.json`, (err, success) => {
     if (err) {
       console.log(err);
     } else {
-      let binArr = JSON.parse(success);
+      const binArr = JSON.parse(success);
       for (let bin of binArr) {
         if (bin.originalUrl === url) {
           res.send({
             original_url: bin.originalUrl,
             short_url: bin.shortUrl,
           });
-        } else {
-          next();
+          fs.writeFileSync(`bins/bin.json`, binArr);
         }
       }
     }
   });
+  next();
+}
+function checkInWeb(req, res, next) {
+  const { body } = req;
+  const url = body.url;
+  res = fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => {
+      console.log(res.status);
+      if (!res.ok) {
+        res.send({ error: "Invalid Hostname" });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log("there was an error", err);
+      res.send({ error: "Invalid Hostname" });
+    });
 }
 module.exports = router;
