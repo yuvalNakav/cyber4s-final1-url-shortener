4d6d824e0c8cfc5b3238b535f71658e535e59bbb
Yuval Nakav <yuvalnakav1@gmail.com>
2021-03-05 17:34:12 +0200

    added a checking middleware for post method

--- models/dataBaseObj.js
+++ models/dataBaseObj.js
@@ -1,6 +1,6 @@
 let id = 1;
 class DataBase {
-  constructor(redirectCount, originalUrl, shortUrl = id) {
+  constructor(originalUrl, shortUrl = id, redirectCount = 0) {
     this.creationDate = new Date().getTime();
     this.redirectCount = redirectCount;
     this.originalUrl = originalUrl;
--- routes/shortUrl/index.js
+++ routes/shortUrl/index.js
@@ -1,34 +1,61 @@
+const { Console } = require("console");
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const path = require("path");
 const DataBase = require("../../models/dataBaseObj");
-// let x = new DataBase(0, `https://facebook.com`);
-// let y = new DataBase(0, `https://facebook.com`);
-// console.log(x, y);
 router.use(express.json());
 
 router.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "../../views") });
 });
-router.get("/new");
+router.get("/new", (req, res) => {});
 
-router.post("/new", (req, res) => {
+router.post("/new", checkExistence, (req, res) => {
   const { body } = req;
   const url = body.url;
-  const urlObject = new DataBase(0, url);
-  fs.writeFile(
-    `bins/${urlObject.shortUrl}.json`,
-    JSON.stringify(urlObject, null, 4),
-    (err) => {
-      if (err) {
-        console.log("there was en error " + err);
-      } else {
-        console.log(
-          urlObject.originalUrl + " was shortened to: " + urlObject.shortUrl
-        );
+  fs.readFile(`bins/bin.json`, (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let binArr = JSON.parse(success);
+      const urlObject = new DataBase(url, binArr.length + 1);
+      binArr.push(urlObject);
+      fs.writeFile(`bins/bin.json`, JSON.stringify(binArr, null, 4), (err) => {
+        if (err) {
+          console.log("there was en error " + err);
+        } else {
+          console.log(
+            urlObject.originalUrl + " was shortened to: " + urlObject.shortUrl
+          );
+        }
+      });
+      res.send({
+        original_url: urlObject.originalUrl,
+        short_url: urlObject.shortUrl,
+      });
+    }
+  });
+});
+function checkExistence(req, res, next) {
+  const { body } = req;
+  const url = body.url;
+  fs.readFile(`bins/bin.json`, (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let binArr = JSON.parse(success);
+      for (let bin of binArr) {
+        if (bin.originalUrl === url) {
+          res.send({
+            original_url: bin.originalUrl,
+            short_url: bin.shortUrl,
+          });
+        } else {
+          next();
+        }
       }
     }
-  );
-});
+  });
+}
 module.exports = router;
